d0ec52a34d173ab5fff95ef303eee1985faf025c
Drew <drewclementsdesign@gmail.com>
2022-12-08 21:05:19 -0500

    more changes

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4 lg:px-0 mt-12">
+  <div class="container mx-auto md:w-1/2 mb-16 lg:mb-32 px-4 lg:px-0 mt-12">
     <h1 class="text-lg uppercase text-gray-400 tracking-widest font-bold mb-4">Who we are &amp; how we work</h1>
     <p class="mb-3">
       Our painters are selected for their craft skills, eye for detail, and commitment to working with others. Painting
